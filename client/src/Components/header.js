c02ba82d425ac35a0958f16da5acfd03b8a75f2c
bjmontillon <pangilinanbj@gmail.com>
2021-11-20 23:10:10 +0800

    added link in header micro-lend

--- client/src/Components/header.js
+++ client/src/Components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
 import { Typography }  from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles ({
     headerContainer: {
@@ -26,7 +27,7 @@ function Header (props) {
     return (
         <div className={classes.headerContainer}  >
             <nav className={classes.navbar} >
-                <div><Typography color='black' variant='h1'>Micro-Lend</Typography></div>
+                <div><Link path='/home' style={{textDecoration: 'none'}}><Typography color='black' variant='h1'>Micro-Lend</Typography></Link></div>
                 <div><Typography color='black' variant='h1'>Beejay P.</Typography></div>
             </nav>
         </div>
