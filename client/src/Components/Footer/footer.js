a9afd6f2729a9aed0d8d186ddeeca3de28bddf0b
bjmontillon <pangilinanbj@gmail.com>
2021-11-24 20:16:23 +0800

    added elements

--- client/src/Components/Footer/footer.js
+++ client/src/Components/Footer/footer.js
@@ -1,22 +1,42 @@
 import {makeStyles} from '@mui/styles'
+import {Grid} from '@mui/material'
+import {Link} from 'react-router-dom'
 
 const useStyles = makeStyles ({
     footerContainer: {
         height: '25vh',
         width: '100%',
         display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
         position: 'relative',
         bottom: 0,
         borderTop: '1px solid black',
-    }
+    },
+    footerLinkWrapper: {
+        width: '100%',
+        height: '20%',
+        display: 'flex',
+        justifyContent: 'flex-end'
+    },
+    footerLinks: {
+        display: 'flex',
+        justfyContent: 'center',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+    },
 })
 const Footer = () => {
     const classes = useStyles()
     return (
         <div className={classes.footerContainer}>
-            <p>footer</p>
+        <div className={classes.footerLinkWrapper}>
+            <Grid item lg={2} className={classes.footerLinks}>
+                <div>
+                    <Link>FB</Link>
+                    <Link>twitter</Link>
+                    <Link>github</Link>
+                </div>
+            </Grid>
+            </div>
         </div>
     )
 }
